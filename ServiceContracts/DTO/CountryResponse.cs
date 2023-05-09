0a72201964b0fa608644e88236f923a2435ed06c
Ahmet U <57094137+GitAhmet@users.noreply.github.com>
2023-05-09 21:36:03 +0200

    DTOEntitiesServiceContracts

---
+++ ServiceContracts/DTO/CountryResponse.cs
@@ -0,0 +1,11 @@
+﻿using Entities;
+
+namespace ServiceContracts.DTO;
+
+/// <summary>
+/// DTO class that is used as return type for most of countries service methods
+/// </summary>
+public class CountryResponse
+{
+
+}
