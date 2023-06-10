d03a5414a8412edf7235383a9f6f8957b666208c
Ahmet U <57094137+GitAhmet@users.noreply.github.com>
2023-06-11 00:26:25 +0200

    Persubs

---
+++ CRUDTest/PersonsServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using ServiceContracts;
+using ServiceContracts.DTO;
+using System;
+using System.Collections.Generic;
+using Xunit;
+using Entities;
+using Services;
+
+namespace CRUDTests;
+public class PersonsServiceTest
+{
+    //private fields
+    private readonly IPersonsService _personService;
+
+    //constructor
+    public PersonsServiceTest()
+    {
+        _personService = new PersonsService();
+    }
+}
---
+++ Services/PersonsService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using ServiceContracts;
+using ServiceContracts.DTO;
+using Entities;
+
+namespace Services;
+public class PersonsService : IPersonsService
+{
+    public PersonResponse AddPerson(PersonAddRequest personAddRequest)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<PersonResponse> GetAllPersons()
+    {
+        throw new NotImplementedException();
+    }
+}
