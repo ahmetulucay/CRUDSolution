c3d22a5bb3315b2a051212c860649f2bc7db02a5
Ahmet U <57094137+GitAhmet@users.noreply.github.com>
2023-07-14 16:00:21 +0200

    PersonUpdateRequest

--- ServiceContracts/DTO/PersonResponse.cs
+++ ServiceContracts/DTO/PersonResponse.cs
@@ -2,6 +2,7 @@
 using System.Net;
 using System.Reflection;
 using Entities;
+using ServiceContracts.Enums;
 
 namespace ServiceContracts.DTO;
 
@@ -56,9 +57,22 @@ public class PersonResponse
             $"Date of Birth: {DateOfBirth?.ToString("dd MMM yyyy")}, Gender: {Gender}, " +
             $"Country ID: {CountryID}, Country :{Country}, Address: {Address}, " +
             $"Receive News Letters: {ReceiveNewsLetters}";
-
     }
 
+    public PersonUpdateRequest ToPersonUpdateRequest()
+    {
+        return new PersonUpdateRequest()
+        {
+            PersonID = PersonID,
+            PersonName = PersonName,
+            Email = Email,
+            DateOfBirth = DateOfBirth,
+            Gender = (GenderOptions)Enum.Parse(typeof(GenderOptions), Gender, true),
+            Address = Address,
+            CountryID = CountryID,
+            ReceiveNewsLetters = ReceiveNewsLetters
+        };
+    }
 }
 
 /// <summary>
