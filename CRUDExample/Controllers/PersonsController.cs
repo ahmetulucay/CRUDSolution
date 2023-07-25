a0c54f981d0cb9ac9da1e422fb8c4246cf608912
Ahmet U <57094137+GitAhmet@users.noreply.github.com>
2023-07-25 16:05:16 +0200

    ListView&PersonIndex

--- CRUDExample/Controllers/PersonsController.cs
+++ CRUDExample/Controllers/PersonsController.cs
@@ -17,8 +17,17 @@ namespace CRUDExample.Controllers
 
         [Route("persons/index")]
         [Route("/")]
-        public IActionResult Index()
+        public IActionResult Index(string searchBy, string? searchString)
         {
+            ViewBag.SearchFields = new Dictionary<string, string>()
+            {
+                {nameof(PersonResponse.PersonName),"Person Name" },
+                {nameof(PersonResponse.Email),"Email" },
+                {nameof(PersonResponse.DateOfBirth),"Date of Birth" },
+                {nameof(PersonResponse.Gender),"Gender(M/F)" },
+                {nameof(PersonResponse.CountryID),"Country" },
+                {nameof(PersonResponse.Address),"Address" }
+            };
             List<PersonResponse> persons = _personsService.GetAllPersons();
 
             return View(persons); //Views/Persons/Index.cshtml
