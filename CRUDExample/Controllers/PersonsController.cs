4edd590f557dc63c1a9790e34fb3316b6002689e
Ahmet U <57094137+GitAhmet@users.noreply.github.com>
2023-08-02 20:05:56 +0200

    CreatePerson

--- CRUDExample/Controllers/PersonsController.cs
+++ CRUDExample/Controllers/PersonsController.cs
@@ -9,11 +9,13 @@ namespace CRUDExample.Controllers
     {
         //private fields
         private readonly IPersonsService _personsService;
+        private readonly ICountriesService _countriesService;
 
         //constructor
-        public PersonsController(IPersonsService personsService)
+        public PersonsController(IPersonsService personsService, ICountriesService countriesService)
         {
             _personsService = personsService;
+            _countriesService = countriesService;
         }
 
         [Route("persons/index")]
@@ -42,6 +44,17 @@ namespace CRUDExample.Controllers
             ViewBag.CurrentSortOrder = sortOrder.ToString();
 
             return View(sortedPersons); //Views/Persons/Index.cshtml
+        }
+
+        //Executes when the user clicks on "Create Person" hyperlink
+        //(while opening the create view)
+        [Route("persons/create")]
+        [HttpGet]
+        public IActionResult Create()
+        {
+            List<CountryResponse> countries = _countriesService.GetAllCountries();
+            ViewBag.Countries = countries;
+            return View();
         }
     }
 }
