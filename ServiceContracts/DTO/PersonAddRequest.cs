df01a831e24a982b49969b0c6588e83d3458ed07
Ahmet U <57094137+GitAhmet@users.noreply.github.com>
2023-08-07 13:15:21 +0200

    TagHelpersAspFor

--- ServiceContracts/DTO/PersonAddRequest.cs
+++ ServiceContracts/DTO/PersonAddRequest.cs
@@ -18,7 +18,9 @@ public class PersonAddRequest
     public string? PersonName { get; set; }
     [Required(ErrorMessage = "Email can't be blank")]
     [EmailAddress(ErrorMessage = "Email value should be a valid email")]
+    [DataType(DataType.EmailAddress)]
     public string? Email { get; set; }
+    [DataType(DataType.Date)]
     public DateTime? DateOfBirth { get; set; }
     public GenderOptions? Gender { get; set; }
     public Guid? CountryID { get; set; }
