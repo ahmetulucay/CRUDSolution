e0f1eb5bd4dda5df668effe005f8ee0df39445ff
Ahmet U <57094137+GitAhmet@users.noreply.github.com>
2023-08-19 13:43:03 +0200

    EditMethod2HttpPost

--- CRUDExample/Controllers/PersonsController.cs
+++ CRUDExample/Controllers/PersonsController.cs
@@ -73,7 +73,12 @@ namespace CRUDExample.Controllers
             if (!ModelState.IsValid)
             {
                 List<CountryResponse> countries = _countriesService.GetAllCountries();
-                ViewBag.Countries = countries;
+                ViewBag.Countries = countries.Select(temp => new SelectListItem()
+                {
+                    Text = temp.CountryName,
+                    Value = temp.CountryID.ToString()
+                });
+
                 ViewBag.Errors = ModelState.Values.SelectMany(v => v.Errors).Select(e=>
                 e.ErrorMessage).ToList();
                 return View();
@@ -91,7 +96,53 @@ namespace CRUDExample.Controllers
         [Route("[action]/{personID}")] //Eg:  /persons/edit/1
         public IActionResult Edit(Guid personID)
         {
-            return View();
+            PersonResponse? personResponse = _personsService.GetPersonByPersonID(personID);
+            if(personResponse == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            PersonUpdateRequest personUpdateRequest = personResponse.ToPersonUpdateRequest();
+
+            List<CountryResponse> countries = _countriesService.GetAllCountries();
+            ViewBag.Countries = countries.Select(temp => new SelectListItem()
+            {
+                Text = temp.CountryName,
+                Value = temp.CountryID.ToString()
+            });
+
+            return View(personUpdateRequest);
+        }
+
+        [HttpPost]
+        [Route("[action]/{personID}")]
+        public IActionResult Edit(PersonUpdateRequest personUpdateRequest)
+        {
+            PersonResponse? personResponse = _personsService.GetPersonByPersonID(personUpdateRequest.PersonID);
+
+            if(personResponse == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            if(ModelState.IsValid)
+            {
+               PersonResponse updatedPerson = _personsService.UpdatePerson(personUpdateRequest);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                List<CountryResponse> countries = _countriesService.GetAllCountries();
+                ViewBag.Countries = countries.Select(temp => new SelectListItem()
+                {
+                    Text = temp.CountryName,
+                    Value = temp.CountryID.ToString()
+                });
+
+                ViewBag.Errors = ModelState.Values.SelectMany(v => v.Errors).Select(e =>
+                e.ErrorMessage).ToList();
+                return View();
+            }
         }
     }
 }
