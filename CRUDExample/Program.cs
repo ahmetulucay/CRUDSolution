6771e86e641cbab6cef5198017228d1f1b5de613
Ahmet U <57094137+GitAhmet@users.noreply.github.com>
2023-08-26 20:02:17 +0200

    CreatingSqlTable

--- CRUDExample/Program.cs
+++ CRUDExample/Program.cs
@@ -17,6 +17,14 @@ builder.Services.AddDbContext<PersonsDbContext>(options =>
     options.UseSqlServer();
 });
 
+//Data Source=(localdb)\MSSQLLocalDB;
+//Initial Catalog=PersonsDatabase;
+//Integrated Security=True;
+//Connect Timeout=30;Encrypt=False;
+//Trust Server Certificate=False;
+//Application Intent=ReadWrite;
+//Multi Subnet Failover=False
+
 var app = builder.Build();
 
 if (builder.Environment.IsDevelopment())
