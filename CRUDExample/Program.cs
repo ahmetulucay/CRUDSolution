f2ad7b4ebc715ec8700909dbdb2d07e1c5982c88
Ahmet U <57094137+GitAhmet@users.noreply.github.com>
2023-09-03 21:24:20 +0200

    AddSingletoneToAddScoped

--- CRUDExample/Program.cs
+++ CRUDExample/Program.cs
@@ -9,8 +9,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 //add services into IoC Container
-builder.Services.AddSingleton<ICountriesService, CountriesService>();
-builder.Services.AddSingleton<IPersonsService, PersonsService>();
+builder.Services.AddScoped<ICountriesService, CountriesService>();
+builder.Services.AddScoped<IPersonsService, PersonsService>();
 
 builder.Services.AddDbContext<PersonsDbContext>(options =>
 {
