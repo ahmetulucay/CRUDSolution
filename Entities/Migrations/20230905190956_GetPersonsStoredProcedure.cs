624d111755018d1a30c6620e28cb6be7e059d43a
Ahmet U <57094137+GitAhmet@users.noreply.github.com>
2023-09-06 23:07:23 +0200

    AddMigrationUp&Downin

--- Entities/Migrations/20230905190956_GetPersonsStoredProcedure.cs
+++ Entities/Migrations/20230905190956_GetPersonsStoredProcedure.cs
@@ -10,13 +10,21 @@ namespace Entities.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-
+            string sp_GetAllPersons = @"
+            CREATE PROCEDURE [dbo].[GetAllPersons]
+            AS BEGIN
+               SELECT PersonID, PersonName, Email, DateOfBirth, Gender,
+               CountryID, Address, ReceiveNewsLetters, FROM [dbo].[Persons]
+            END";
+            migrationBuilder.Sql(sp_GetAllPersons);
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-
+            string sp_GetAllPersons = @"
+            DROP PROCEDURE[dbo].[GetAllPersons]";
+            migrationBuilder.Sql(sp_GetAllPersons);
         }
     }
 }
