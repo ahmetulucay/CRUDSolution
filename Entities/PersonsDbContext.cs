3bfdec82924e59e2a51e43887a7e332943e25d98
Ahmet U <57094137+GitAhmet@users.noreply.github.com>
2023-09-13 19:33:46 +0200

    HasCheckConstraint_CHK_TIN

--- Entities/PersonsDbContext.cs
+++ Entities/PersonsDbContext.cs
@@ -46,6 +46,11 @@ public class PersonsDbContext : DbContext
             .HasColumnName("TaxIdentificationNumber")
             .HasColumnType("varchar(8)")
             .HasDefaultValue("ABC12345");
+
+        //modelBuilder.Entity<Person>().HasIndex(temp => temp.TIN).IsUnique();
+
+        modelBuilder.Entity<Person>()
+            .HasCheckConstraint("CHK_TIN", "len([TaxIdentificationNumber]) = 8");
     }
 
     public List<Person> sp_GetAllPersons()
