6906de19b2ca0ecb56077bdaf2b2cbe273255479
Ahmet U <57094137+GitAhmet@users.noreply.github.com>
2023-10-24 10:40:14 +0200

    CreatingAddCountryTask

--- RepsositoryContracts/ICountriesRepository.cs
+++ RepsositoryContracts/ICountriesRepository.cs
@@ -1,7 +1,17 @@
-﻿namespace RepositoryContracts
+﻿using Entities;
+
+namespace RepositoryContracts
 {
+    /// <summary>
+    /// Represents data access logic for managing Person entity
+    /// </summary>
     public interface ICountriesRepository
     {
-
+        /// <summary>
+        /// Adds a new country object to the data store
+        /// </summary>
+        /// <param name="country">Country object to add</param>
+        /// <returns>Returns the country object after adding it to the data store</returns>
+        Task<Country> AddCountry (Country country);
     }
 }
