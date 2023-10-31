c2cab292b7c54b58f68b58647397f987b019b469
Ahmet U <57094137+GitAhmet@users.noreply.github.com>
2023-10-31 15:02:53 +0100

    CreatingCountriesRepository

---
+++ Repositories/CountriesRepository.cs
@@ -0,0 +1,27 @@
+﻿using Entities;
+using RepositoryContracts;
+
+namespace Repositories;
+
+public class CountriesRepository : ICountriesRepository
+{
+    public Task<Country> AddCountry(Country country)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<Country>> GetAllCountries()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Country?> GetCountryByCountryId(Guid countryID)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Country?> GetCountryByCountryName(string countryName)
+    {
+        throw new NotImplementedException();
+    }
+}
