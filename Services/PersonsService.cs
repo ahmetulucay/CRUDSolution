a64bc4d0a834bcbf5cc8371e395f541624cdccd6
Ahmet U <57094137+GitAhmet@users.noreply.github.com>
2023-11-11 12:54:29 +0100

    GelFilteredPersonsUpdating2

--- Services/PersonsService.cs
+++ Services/PersonsService.cs
@@ -202,7 +202,7 @@ public class PersonsService : IPersonsService
         ValidationHelper.ModelValidation(personUpdateRequest);
 
         //get matching person object to update
-        Person? matchingPerson = await _personsRepository.Persons.FirstOrDefaultAsync(temp => temp.PersonID == personUpdateRequest.PersonID);
+        Person? matchingPerson = await _personsRepository.GetPersonByPersonId(personUpdateRequest.PersonID);
         if (matchingPerson == null)
         {
             throw new ArgumentException("Given person id doesn't exist");
@@ -217,7 +217,7 @@ public class PersonsService : IPersonsService
         matchingPerson.Address = personUpdateRequest.Address;
         matchingPerson.ReceiveNewsLetters = personUpdateRequest.ReceiveNewsLetters;
 
-        await _personsRepository.SaveChangesAsync(); //UPDATE
+        await _personsRepository.UpdatePerson(matchingPerson); //UPDATE
 
         return matchingPerson.ToPersonResponse();
     }
@@ -229,13 +229,12 @@ public class PersonsService : IPersonsService
             throw new ArgumentNullException(nameof(personID));
         }
 
-        Person? person = await _personsRepository.Persons.FirstOrDefaultAsync(temp => temp.PersonID == personID);
+        Person? person = await _personsRepository.GetPersonByPersonId(personID.Value);
 
         if (person == null)
             return false;
 
-        _personsRepository.Persons.Remove(_personsRepository.Persons.First(temp => temp.PersonID == personID));
-        await _personsRepository.SaveChangesAsync();
+        await _personsRepository.DeletePersonByPersonID(personID.Value);
 
         return true;
     }
@@ -259,9 +258,7 @@ public class PersonsService : IPersonsService
         csvWriter.WriteField(nameof(PersonResponse.ReceiveNewsLetters));
         csvWriter.NextRecord();
 
-        List<PersonResponse> persons = _personsRepository.Persons
-            .Include("Country")
-            .Select(tmp => tmp.ToPersonResponse()).ToList();
+        List<PersonResponse> persons = await GetAllPersons();
 
         foreach (PersonResponse person in persons) 
         {
@@ -310,8 +307,7 @@ public class PersonsService : IPersonsService
             }
 
             int row = 2;
-            List<PersonResponse> persons = _personsRepository.Persons.Include("Country").Select(
-                temp => temp.ToPersonResponse()).ToList();
+            List<PersonResponse> persons = await GetAllPersons();
             foreach (PersonResponse person in persons)
             {
                 excelWorksheet.Cells[row, 1].Value = person.PersonName;
