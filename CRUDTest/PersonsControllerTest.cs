58151f67592d1308cb9f6a4d0ead64832939fefb
Ahmet U <57094137+GitAhmet@users.noreply.github.com>
2023-12-04 14:08:10 +0100

    CreatingControllerTestClass

---
+++ CRUDTest/PersonsControllerTest.cs
@@ -0,0 +1,28 @@
+﻿using AutoFixture;
+using Moq;
+using ServiceContracts;
+
+namespace CRUDTests;
+public class PersonsControllerTest
+{
+    private readonly IPersonsService _personsService;
+    private readonly ICountriesService _countriesService;
+
+    private readonly Mock<IPersonsService> _personsServiceMock;
+    private readonly Mock<ICountriesService> _countriesServiceMock;
+
+    private readonly Fixture _fixture;
+
+    public PersonsControllerTest()
+    {
+        _fixture = new Fixture();
+
+        _countriesServiceMock = new Mock<ICountriesService>();
+        _personsServiceMock = new Mock<IPersonsService>(); 
+
+        _countriesService = _countriesServiceMock.Object;
+        _personsService = _personsServiceMock.Object;
+    }
+}
+
+
