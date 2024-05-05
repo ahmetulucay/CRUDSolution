a43dff5e783fdbd96a2fad3935c1c79c44375700
Ahmet U <57094137+GitAhmet@users.noreply.github.com>
2024-05-05 16:55:37 +0200

    PersonsListActionFilter

---
+++ CRUDExample/Filters/ActionFilters/PersonsListActionFilter.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace CRUDExample.Filters.ActionFilters;
+public class PersonsListActionFilter : IActionFilter
+{
+    public void OnActionExecuted(ActionExecutedContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnActionExecuting(ActionExecutingContext context)
+    {
+        throw new NotImplementedException();
+    }
+}
