5cc25f481fa3443ce0d3e9bc9f88874be1ed9628
Ahmet U <57094137+GitAhmet@users.noreply.github.com>
2024-05-10 13:49:21 +0200

    PersonsListActionFilter2

--- CRUDExample/Filters/ActionFilters/PersonsListActionFilter.cs
+++ CRUDExample/Filters/ActionFilters/PersonsListActionFilter.cs
@@ -3,13 +3,21 @@
 namespace CRUDExample.Filters.ActionFilters;
 public class PersonsListActionFilter : IActionFilter
 {
+    private readonly ILogger<PersonsListActionFilter> _logger;
+
+    public PersonsListActionFilter(ILogger<PersonsListActionFilter> logger)
+    {
+        _logger = logger;
+    }
     public void OnActionExecuted(ActionExecutedContext context)
     {
-        throw new NotImplementedException();
+        //To do: add after logic here
+        _logger.LogInformation("PersonsListActionFilter.OnActionExecuted method");
     }
 
     public void OnActionExecuting(ActionExecutingContext context)
     {
-        throw new NotImplementedException();
+        //To do: add before logic here
+        _logger.LogInformation("PersonsListActionFilter.OnActionExecuting method");
     }
 }
